74e28bc448b9304b69beeb28b7e6652a7ccdc936
moise10r <moiserushanika2006@gmail.com>
2021-07-01 22:33:05 +0200

    Add the setup of the project.

--- src/components/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Main = () => {
-    return (<h1>Hello</h1>  );
-}
- 
-export default Main;
---
+++ src/components/navbar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+const NavBar = () => {
+  return ( <nav className="main-navbar">
+        <div className="main-navbar-container">NavBar</div>
+  </nav> );
+}
+ 
+export default NavBar;
