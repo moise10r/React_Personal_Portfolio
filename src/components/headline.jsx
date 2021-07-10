27db9e61319f157e9b334db4bc3ebc03fe425452
moise10r <moiserushanika2006@gmail.com>
2021-07-10 22:44:58 +0200

    Add the styling of the headline section.

--- src/components/headline.jsx
+++ src/components/headline.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
+import { IconContext } from 'react-icons';
+import { AiOutlineDisconnect } from 'react-icons/ai';
+
 const HeadLine = () => {
 	return (
 		<section className='headline-section'>
 			<div className='main-healine-container'>
 				<h1 className='me'>
-					I'm Moise Glad to see you !<span>.</span>
+					I'm Moise Glad to see you <span>!</span>
 				</h1>
-				<p>
+				<p className='about-me'>
 					I’m a software developer! I can help you build a product , feature or
 					website Look through some of my work and experience! If you like what
 					you see and have a project you need coded, don’t hestiate to contact
 					me.
 				</p>
+				<div className='connections'>
+					<a href='#2' className='btn-resume'>
+						Resume
+					</a>
+					<a href='#2' className='connect'>
+						LET'S CONNECT{' '}
+						<span>
+							<IconContext.Provider value={{ className: 'connect-icon' }}>
+								<AiOutlineDisconnect />
+							</IconContext.Provider>
+						</span>
+					</a>
+				</div>
 			</div>
 		</section>
 	);
