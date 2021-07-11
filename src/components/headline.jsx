e669c7642421cfe6a5a5a3210127a4c3ceb45187
moise10r <moiserushanika2006@gmail.com>
2021-07-11 02:09:42 +0200

    Add the svg headline and refactor its design.

--- src/components/headline.jsx
+++ src/components/headline.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
 import { IconContext } from 'react-icons';
 import { AiOutlineDisconnect } from 'react-icons/ai';
+import headlineImg from '../assets/images/headline_bg.svg';
 
 const HeadLine = () => {
 	return (
 		<section className='headline-section'>
 			<div className='main-healine-container'>
-				<h1 className='me'>
-					I'm Moise Glad to see you <span>!</span>
-				</h1>
-				<p className='about-me'>
-					I’m a software developer! I can help you build a product , feature or
-					website Look through some of my work and experience! If you like what
-					you see and have a project you need coded, don’t hestiate to contact
-					me.
-				</p>
-				<div className='connections'>
-					<a href='#2' className='btn-resume'>
-						Resume
-					</a>
-					<a href='#2' className='connect'>
-						LET'S CONNECT{' '}
-						<span>
-							<IconContext.Provider value={{ className: 'connect-icon' }}>
-								<AiOutlineDisconnect />
-							</IconContext.Provider>
-						</span>
-					</a>
+				<div className='headline-left-container'>
+					<h1 className='me'>
+						I'm Moise Glad to see you <span>!</span>
+					</h1>
+					<p className='about-me'>
+						I’m a software developer! I can help you build a product , feature
+						or website Look through some of my work and experience! If you like
+						what you see and have a project you need coded, don’t hestiate to
+						contact me.
+					</p>
+					<div className='connections'>
+						<a href='#2' className='btn-resume'>
+							Resume
+						</a>
+						<a href='#2' className='connect'>
+							LET'S CONNECT{' '}
+							<span>
+								<IconContext.Provider value={{ className: 'connect-icon' }}>
+									<AiOutlineDisconnect />
+								</IconContext.Provider>
+							</span>
+						</a>
+					</div>
+				</div>
+				<div className='headline-img'>
+					<img src={headlineImg} alt='' />
 				</div>
 			</div>
 		</section>
