161d4a92fd98e2a37c972794f680dc605fb6d747
moise10r <moiserushanika2006@gmail.com>
2021-07-11 05:17:02 +0200

    Add the toggle navigation style.

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -5,6 +5,8 @@ import { IoMdMenu } from 'react-icons/io';
 import { FaFacebookF } from 'react-icons/fa';
 import { FiTwitter } from 'react-icons/fi';
 import { GrLinkedinOption } from 'react-icons/gr';
+import { VscClose } from 'react-icons/vsc';
+import navbarImg from '../assets/images/navbar-img.svg';
 
 class Header extends Component {
 	state = {};
@@ -24,50 +26,79 @@ class Header extends Component {
 									<IoMdMenu />
 								</IconContext.Provider>
 							</div>
-							<ul className='nav-list'>
-								<li className='nav-link'>
-									<a href='#1'>Home</a>
-								</li>
-								<li className='nav-link'>
-									<a href='#2'>Portfolio</a>
-								</li>
-								<li className='nav-link'>
-									<a href='#3'>About</a>
-								</li>
-								<li className='nav-link'>
-									<a href='#4'>Contact</a>
-								</li>
-							</ul>
-							<ul className='social-media-list'>
-								<li className='social-media-link'>
-									<a href='#2'>
-										<IconContext.Provider value={{ className: 'header-icon' }}>
-											<FaFacebookF />
-										</IconContext.Provider>
-									</a>
-								</li>
-								<li className='social-media-link'>
-									<a href='#2'>
-										<IconContext.Provider value={{ className: 'header-icon' }}>
-											<ImGithub />
-										</IconContext.Provider>
-									</a>
-								</li>
-								<li className='social-media-link'>
-									<a href='#2'>
-										<IconContext.Provider value={{ className: 'header-icon' }}>
-											<FiTwitter />
-										</IconContext.Provider>
-									</a>
-								</li>
-								<li className='social-media-link'>
-									<a href='#2'>
-										<IconContext.Provider value={{ className: 'header-icon' }}>
-											<GrLinkedinOption />
-										</IconContext.Provider>
-									</a>
-								</li>
-							</ul>
+							<div className='close-icon'>
+								<IconContext.Provider value={{ className: 'close' }}>
+									<VscClose />
+								</IconContext.Provider>
+							</div>
+							<div className='header-list'>
+								<div className='navbar-img-wrapper'>
+									<img src={navbarImg} alt='' srcset='' />
+								</div>
+								<ul className='nav-list'>
+									<li className='nav-link'>
+										<a href='#1'>Home</a>
+									</li>
+									<li className='nav-link'>
+										<a href='#2'>Portfolio</a>
+									</li>
+									<li className='nav-link'>
+										<a href='#3'>About</a>
+									</li>
+									<li className='nav-link'>
+										<a href='#4'>Contact</a>
+									</li>
+								</ul>
+								<ul className='social-media-list'>
+									<li className='social-media-link'>
+										<a href='#2'>
+											<IconContext.Provider
+												value={{ className: 'header-icon' }}
+											>
+												<FaFacebookF />
+											</IconContext.Provider>
+										</a>
+									</li>
+									<li className='social-media-link'>
+										<a href='#2'>
+											<IconContext.Provider
+												value={{ className: 'header-icon' }}
+											>
+												<ImGithub />
+											</IconContext.Provider>
+										</a>
+									</li>
+									<li className='social-media-link'>
+										<a href='#2'>
+											<IconContext.Provider
+												value={{ className: 'header-icon' }}
+											>
+												<FiTwitter />
+											</IconContext.Provider>
+										</a>
+									</li>
+									<li className='social-media-link'>
+										<a href='#2'>
+											<IconContext.Provider
+												value={{ className: 'header-icon' }}
+											>
+												<GrLinkedinOption />
+											</IconContext.Provider>
+										</a>
+									</li>
+								</ul>
+								<ul className='languages'>
+									<li>
+										<a href='#2'>FR</a>
+									</li>
+									<li>
+										<a href='#2'>EN</a>
+									</li>
+									<li>
+										<a href='#3'>moiserushanika2006@gmail.com</a>
+									</li>
+								</ul>
+							</div>
 						</div>
 					</nav>
 				</div>
