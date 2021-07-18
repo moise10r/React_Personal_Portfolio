0176e3d0d459d924060673cd1d3aa265160f109d
moise10r <moiserushanika2006@gmail.com>
2021-07-18 11:30:43 +0200

    Add the loading screen

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import Home from './components/pages/home';
 import './assets/css/main.css';
+import {useEffect, useState} from 'react';
+import BounceLoader from "react-spinners/BounceLoader";
+
 function App() {
+	const [loading,setLoading] = useState(false);
+	useEffect(() => {
+		setLoading(true);
+		setTimeout(() => {
+		setLoading(false);
+		}, 8000);
+	}, []);
 	return (
 		<div className='App'>
-			<Home />
+			{loading ?( 
+      <div className="loader">
+				<BounceLoader color={'#FD0321'} loading={loading} size={60} />
+			</div>) : (<Home />)}
+			
 		</div>
 	);
 }
