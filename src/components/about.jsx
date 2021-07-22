c0548a4f9c594ead96c6f93f4954959553485b5b
moise10r <moiserushanika2006@gmail.com>
2021-07-22 21:38:52 +0200

    Addorp down langages and frameworks list.

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { IconContext } from 'react-icons';
 import { ImGithub } from 'react-icons/im';
 import { FaFacebookF } from 'react-icons/fa';
 import { FiTwitter } from 'react-icons/fi';
 import { GrLinkedinOption } from 'react-icons/gr';
 import { FiChevronDown } from 'react-icons/fi';
+import { FiChevronRight } from 'react-icons/fi';
 import { HiChevronDoubleLeft } from 'react-icons/hi';
 import { IoMdPlay} from 'react-icons/io';
 import ellipse_1 from '../assets/images/Ellipse_1.png';
@@ -13,150 +14,226 @@ import ellipse_3 from '../assets/images/Ellipse_3.png';
 import about from '../assets/images/about.jpg';
 
 
-const About = () => {
-	return (
-		<section className='about'>
-			<div className='about-me-main-section'>
-				<h2 className='title'>
-					About me<span>.</span>
-				</h2>
-				<div className='main-about-wrapper-one'>
-					<div className='about-left-wrapper'>
-						<p className='about-me-details'>
-							Hello I’m a software developer! I can help you build a product ,
-							feature or website Look through some of my work and experience! If
-							you like what you see and have a project you need coded, don’t
-							hestiate to contact me.
-						</p>
-						<a href='#2' className='about-connect'>
-							<span>Let's connect</span>
-							<span>
-								<IconContext.Provider
-									value={{ className: 'about-connect-icon' }}
-								>
-									<HiChevronDoubleLeft />
-								</IconContext.Provider>
-							</span>
-						</a>
-						<ul className='about-social-icon-list'>
-							<li className='social-media-link'>
-								<a href='#2'>
-									<IconContext.Provider value={{ className: 'about-icon' }}>
-										<FaFacebookF />
+class About extends Component {
+		state = {
+			isLangOpen: false,
+			isFramOpen: false
+		}
+	
+	handleFrames = () => {
+		console.log('click');
+		if(!this.state.isFramOpen) {
+			this.setState({
+				isFramOpen:true
+			})
+			console.log(this.state.isFramOpen);
+		} else {
+			this.setState({
+				isFramOpen:false
+			})
+			console.log(this.state.isFramOpen);
+
+		}
+	}
+	handleLangs= () => {
+		console.log('click');
+		if(!this.state.isLangOpen) {
+			this.setState({
+				isLangOpen:true
+			})
+			console.log(this.state.isLangOpen);
+		} else {
+			this.setState({
+				isLangOpen:false
+			})
+			console.log(this.state.isLangOpen);
+
+		}
+	}
+	render(){
+		const { isLangOpen, isFramOpen } = this.state;
+		return (
+			<section className='about'>
+				<div className='about-me-main-section'>
+					<h2 className='title'>
+						About me<span>.</span>
+					</h2>
+					<div className='main-about-wrapper-one'>
+						<div className='about-left-wrapper'>
+							<p className='about-me-details'>
+								Hello I’m a software developer! I can help you build a product ,
+								feature or website Look through some of my work and experience! If
+								you like what you see and have a project you need coded, don’t
+								hestiate to contact me.
+							</p>
+							<a href='#2' className='about-connect'>
+								<span>Let's connect</span>
+								<span>
+									<IconContext.Provider
+										value={{ className: 'about-connect-icon' }}
+									>
+										<HiChevronDoubleLeft />
 									</IconContext.Provider>
-								</a>
+								</span>
+							</a>
+							<ul className='about-social-icon-list'>
+								<li className='social-media-link'>
+									<a href='#2'>
+										<IconContext.Provider value={{ className: 'about-icon' }}>
+											<FaFacebookF />
+										</IconContext.Provider>
+									</a>
+								</li>
+								<li className='social-media-link'>
+									<a href='#2'>
+										<IconContext.Provider value={{ className: 'about-icon' }}>
+											<ImGithub />
+										</IconContext.Provider>
+									</a>
+								</li>
+								<li className='social-media-link'>
+									<a href='#2'>
+										<IconContext.Provider value={{ className: 'about-icon' }}>
+											<FiTwitter />
+										</IconContext.Provider>
+									</a>
+								</li>
+								<li className='social-media-link'>
+									<a href='#2'>
+										<IconContext.Provider value={{ className: 'about-icon' }}>
+											<GrLinkedinOption />
+										</IconContext.Provider>
+									</a>
+								</li>
+							</ul>
+						</div>
+						<ul className='about-rigth-wrapper'>
+							<li className='languages-list'>
+								<h3 className='language-header'>
+									<span>Languages</span>
+									<div className="chevron-icons" onClick={this.handleLangs}>
+										{isLangOpen ? (<span>
+											<IconContext.Provider value={{ className: 'about-icon' }}>
+												<FiChevronDown />
+											</IconContext.Provider>
+										</span>):(	
+										<span>
+											<IconContext.Provider value={{ className: 'about-icon' }}>
+												<FiChevronRight />
+											</IconContext.Provider>
+										</span>)}
+									</div>
+								</h3>
+								<ul className={isLangOpen? ("languages-list-container open"):("languages-list-container")}>
+									<li className='language-link'>
+										<img src={ellipse_1} alt='langueage' />
+										<span>JavaScript</span>
+									</li>
+									<li className='language-link'>
+										<img src={ellipse_2} alt='langueage' />
+										<span>Html</span>
+									</li>
+									<li className='language-link'>
+										<img src={ellipse_3} alt='langueage' />
+										<span>Css</span>
+									</li>
+								</ul>
 							</li>
-							<li className='social-media-link'>
-								<a href='#2'>
-									<IconContext.Provider value={{ className: 'about-icon' }}>
-										<ImGithub />
-									</IconContext.Provider>
-								</a>
-							</li>
-							<li className='social-media-link'>
-								<a href='#2'>
-									<IconContext.Provider value={{ className: 'about-icon' }}>
-										<FiTwitter />
-									</IconContext.Provider>
-								</a>
-							</li>
-							<li className='social-media-link'>
-								<a href='#2'>
-									<IconContext.Provider value={{ className: 'about-icon' }}>
-										<GrLinkedinOption />
-									</IconContext.Provider>
-								</a>
+							<li className='languages-list'>
+								<h3 className='language-header'>
+									<span>Frameworks</span>
+									<div className="chevron-icons" onClick={this.handleFrames}>
+										{isFramOpen ? (<span>
+											<IconContext.Provider value={{ className: 'about-icon' }}>
+												<FiChevronDown />
+											</IconContext.Provider>
+										</span>):(	
+										<span>
+											<IconContext.Provider value={{ className: 'about-icon' }}>
+												<FiChevronRight />
+											</IconContext.Provider>
+										</span>)}
+									</div>
+								</h3> 
+								<ul className={isFramOpen? ("languages-list-container open"):("languages-list-container")}>
+									<li className='language-link'>
+										<img src={ellipse_1} alt='langueage' />
+										<span>JavaScript</span>
+									</li>
+									<li className='language-link'>
+										<img src={ellipse_2} alt='langueage' />
+										<span>Html</span>
+									</li>
+									<li className='language-link'>
+										<img src={ellipse_3} alt='langueage' />
+										<span>Css</span>
+									</li>
+								</ul> 
 							</li>
 						</ul>
 					</div>
-					<ul className='about-rigth-wrapper'>
-						<li className='languages-list'>
-							<h3 className='language-header'>
-								<span>Languages</span>
-								<span>
-									<IconContext.Provider value={{ className: 'about-icon' }}>
-										<FiChevronDown />
-									</IconContext.Provider>
-								</span>
-							</h3>
-							<ul className='languages-list-container'>
-								<li className='language-link'>
-									<img src={ellipse_1} alt='langueage' />
-									<span>JavaScript</span>
-								</li>
-								<li className='language-link'>
-									<img src={ellipse_2} alt='langueage' />
-									<span>Html</span>
-								</li>
-								<li className='language-link'>
-									<img src={ellipse_3} alt='langueage' />
-									<span>Css</span>
-								</li>
-							</ul>
-						</li>
-					</ul>
-				</div>
-				<div className='main-about-wrapper-two'>
-					<div className='about-left-main-container'>
-						<div className='experience-year'>
-							<span>2</span>
-							<span className='dot'></span>
-						</div>
-						<div className='year-paragraph'>
-							<div className='left'></div>
-							<div className='right'>
-								<span>Year</span>
-								<span>Experience</span>
-								<span>Working</span>
+					<div className='main-about-wrapper-two'>
+						<div className='about-left-main-container'>
+							<div className='experience-year'>
+								<span>2</span>
+								<span className='dot'></span>
+							</div>
+							<div className='year-paragraph'>
+								<div className='left'></div>
+								<div className='right'>
+									<span>Year</span>
+									<span>Experience</span>
+									<span>Working</span>
+								</div>
 							</div>
 						</div>
-					</div>
-					<div className='about-right-main-container'>
-						<h2 className='great-experience'>Great Experience</h2>
-						<p className='detail'>
-							Fill appear won't may make moveth signs. Fourth. Good own. Green
-							you're moveth us, lesser.
-						</p>
-						<div className='progrees-container'>
-							<div className='progress'>
-								<div className='progress-container'>
-									<span>Web design</span>
-									<span>70%</span>
+						<div className='about-right-main-container'>
+							<h2 className='great-experience'>Great Experience</h2>
+							<p className='detail'>
+								Fill appear won't may make moveth signs. Fourth. Good own. Green
+								you're moveth us, lesser.
+							</p>
+							<div className='progrees-container'>
+								<div className='progress'>
+									<div className='progress-container'>
+										<span>Web design</span>
+										<span>70%</span>
+									</div>
+									<div className='progress-bar'></div>
 								</div>
-								<div className='progress-bar'></div>
-							</div>
-							<div className='progress'>
-								<div className='progress-container'>
-									<span>Electronic</span>
-									<span>85%</span>
+								<div className='progress'>
+									<div className='progress-container'>
+										<span>Electronic</span>
+										<span>85%</span>
+									</div>
+									<div className='progress-bar'></div>
 								</div>
-								<div className='progress-bar'></div>
-							</div>
-							<div className='progress'>
-								<div className='progress-container'>
-									<span>Web design</span>
-									<span>90%</span>
+								<div className='progress'>
+									<div className='progress-container'>
+										<span>Web design</span>
+										<span>90%</span>
+									</div>
+									<div className='progress-bar'></div>
 								</div>
-								<div className='progress-bar'></div>
 							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-			<div className='short-video-section'>
-				<div className='short-video-container'>
-					<div className="about-img-wrapper"><img src={about } alt="bg" /></div>
-					<div className="play-container">
-						<div className='play'>
-							<IconContext.Provider value={{ className: 'about-icon' }}>
-								<IoMdPlay/>
-							</IconContext.Provider>
+				<div className='short-video-section'>
+					<div className='short-video-container'>
+						<div className="about-img-wrapper"><img src={about } alt="bg" /></div>
+						<div className="play-container">
+							<div className='play'>
+								<IconContext.Provider value={{ className: 'about-icon' }}>
+									<IoMdPlay/>
+								</IconContext.Provider>
+							</div>
 						</div>
 					</div>
 				</div>
-			</div>
-		</section>
-	);
+			</section>
+		);
+	}
 };
 
 export default About;
