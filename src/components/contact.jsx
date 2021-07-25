3166ef384ca2c3403ed9fdd86ef9e34f238a5095
moise10r <moiserushanika2006@gmail.com>
2021-07-26 00:41:54 +0200

    Add animation on the contact section.

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -8,18 +8,24 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 class Contact extends Component {
 	state = {};
-componentDidMount() {
-  AOS.init({
-    duration : 2000
-  })
-}
+	componentDidMount() {
+		AOS.init({
+			duration: 2000,
+		});
+	}
 
 	render() {
 		return (
 			<section className='contact-main-section'>
 				<div className='contact-main-wrapper'>
-					<h2 className='title'>Contact</h2>
-					<p className='detail-info'>
+					<h2 data-aos='fade-down-right' className='title'>
+						Contact
+					</h2>
+					<p
+						data-aos='fade-up'
+						data-aos-anchor-placement='top-bottom'
+						className='detail-info'
+					>
 						{' '}
 						If you have an application you are interested in developing, a
 						feature that you need built or a project that needs coding. I’d love
@@ -28,44 +34,31 @@ componentDidMount() {
 					<p className='emojs'>😆 😃 🤩</p>
 					<div className='contact-main-wrapper'>
 						<div className='main-contact-right-container'>
-							<h3>
+							<h3 data-aos="zoom-in-down">
 								Contact Info <span>.</span>{' '}
 							</h3>
-							<p className='show-info'>
+							<p data-aos="zoom-in-right" className='show-info'>
 								Always available for freelance work if the write project comes
 								along, Feel free to contact me
 							</p>
 							<div className='more-info'>
 								<ul className='info-icons'>
-									<li
-                                   data-aos="fade-up-right"
-                                   data-aos-duration="4000"
-                  >
+									<li data-aos='fade-up-right' data-aos-duration='4000'>
 										<IconContext.Provider value={{ className: 'icon' }}>
 											<GiCircleClaws />
 										</IconContext.Provider>
 									</li>
-									<li
-                                   data-aos="fade-up-right"
-                                   data-aos-duration="3000"
-                  >
+									<li data-aos='fade-up-right' data-aos-duration='3000'>
 										<IconContext.Provider value={{ className: 'icon' }}>
 											<MdLocationCity />
 										</IconContext.Provider>
 									</li>{' '}
-									<li
-                                   data-aos="fade-up-right"
-                                   data-aos-duration="2000"
-                  >
+									<li data-aos='fade-up-right' data-aos-duration='2000'>
 										<IconContext.Provider value={{ className: 'icon' }}>
 											<FiPhoneCall />
 										</IconContext.Provider>
 									</li>{' '}
-									<li
-                  data-aos="fade-up-right"
-                  data-aos-duration="1000"
-
-                  >
+									<li data-aos='fade-up-right' data-aos-duration='1000'>
 										<IconContext.Provider value={{ className: 'icon' }}>
 											<IoIosSend />
 										</IconContext.Provider>
@@ -94,24 +87,26 @@ componentDidMount() {
 
 						<div className='main-contact-left-container'>
 							<p>Message Me</p>
-							<form 
-              id='contact-form' action='/'>
-								<div 
-                data-aos='fade-left'
-                data-aos-duration="1000"
-                className='form-group'>
+							<form id='contact-form' action='/'>
+								<div
+									data-aos='fade-left'
+									data-aos-duration='1000'
+									className='form-group'
+								>
 									<input type='text' name='name' placeholder='Your Name' />
 								</div>
-								<div 
-                     data-aos='fade-left'
-                     data-aos-duration="2000"
-                className='form-group'>
+								<div
+									data-aos='fade-left'
+									data-aos-duration='2000'
+									className='form-group'
+								>
 									<input type='text' name='email' placeholder='Your Email' />
 								</div>
-								<div         
-                     data-aos='fade-left'
-                     data-aos-duration="2500"
-                className='form-group'>
+								<div
+									data-aos='fade-left'
+									data-aos-duration='2500'
+									className='form-group'
+								>
 									<textarea
 										name='message'
 										id='message'
@@ -120,10 +115,12 @@ componentDidMount() {
 										placeholder='Message'
 									></textarea>
 								</div>
-								<button 
-                 data-aos="fade-up"
-                 data-aos-anchor-placement="center-bottom"
-                type='submit' className='btn'>
+								<button
+									data-aos='fade-up'
+									data-aos-anchor-placement='center-bottom'
+									type='submit'
+									className='btn'
+								>
 									Get In Touch
 								</button>
 							</form>
